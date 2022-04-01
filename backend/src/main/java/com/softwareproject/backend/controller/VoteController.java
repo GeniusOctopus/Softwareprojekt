fec986f3c20ce29a5932b241be60c58de6710386
GeniusOctopus <k.white221@outlook.com>
2022-04-01 11:02:59 +0200

    [EDIT] added cross origin.

--- backend/src/main/java/com/softwareproject/backend/controller/VoteController.java
+++ backend/src/main/java/com/softwareproject/backend/controller/VoteController.java
@@ -8,15 +8,13 @@ import org.jsondoc.core.annotation.ApiMethod;
 import org.jsondoc.core.annotation.ApiPathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @Api(name = "Vote", description = "Endpunkt für Votes", group = "Vote")
+@CrossOrigin(origins = "http://localhost:4200/")
 @Controller
 @RequestMapping("/vote")
 public class VoteController {
